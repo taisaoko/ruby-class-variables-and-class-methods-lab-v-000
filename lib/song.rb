cb01e0e8a674f0a917a8a79873b2c0735a21da83
taisaoko <ttd3calvin@gmail.com>
2018-08-31 21:52:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@genres << @genre unless @@genres.include?(genre)
+    @@genres << @genre unless @@genres.include?(@genre)
     @@artists << @artist 
   end
   

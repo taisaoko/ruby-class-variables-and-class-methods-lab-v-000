1acebc83721d9bbd3988eaacab2739f201866c85
taisaoko <ttd3calvin@gmail.com>
2018-08-31 21:58:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,11 +41,11 @@ class Song
   
   def self.artist_count
     artists_hash = Hash.new(0)
-    @@artists.each do |genre| 
-      if artists_hash[genre]
-        artists_hash[genre] += 1
+    @@artists.each do |artist| 
+      if artists_hash[artist]
+        artists_hash[artist] += 1
       else 
-        artists_hash[genre] = 1
+        artists_hash[artist] = 1
       end
     end
     artists_hash

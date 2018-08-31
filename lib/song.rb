ce8223292fb604f13feb24f7224b968095f05782
taisaoko <ttd3calvin@gmail.com>
2018-08-31 21:22:14 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << @genre unless @@genres.include?(genre)
+    @@artists << @artist 
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    genres_hash = Hash.new(0)
+    genres_hash.tap { |hash| @@genres.each { |genre| hash[genre] += 1 } }
+  end
+  
+  def self.artist_count
+    artists_hash = Hash.new(0)
+    artists_hash.tap { |hash| @@artists.each { |artist| hash[artist] += 1 } }
+  end
+  
+    
+end
